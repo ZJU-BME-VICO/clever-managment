dafb2de5f8c84fbf1af70ad6739a319d1abc9571
Jacky.Lau <jacky.lauchunkin@gmail.com>
2014-11-07 19:01:37 +0800

    #2 Construct multi module

---
+++ clever-management-service/src/main/java/edu/zju/bme/clever/management/test/MavenProfileInheritance.java
@@ -0,0 +1,15 @@
+package edu.zju.bme.clever.management.test;
+
+public class MavenProfileInheritance {
+
+	private String value;
+
+	public MavenProfileInheritance(String value) {
+		this.value = value;
+	}
+
+	public void printValue() {
+		System.out.println(value);
+	}
+
+}
