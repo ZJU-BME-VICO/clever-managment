763c3dabd9d5017a359dc874d76305c6f327465d
Jacky.Lau <jacky.lauchunkin@gmail.com>
2014-11-10 11:43:23 +0800

    #2 Construct 4 modules.

--- clever-management-web/src/test/java/edu/zju/bme/clever/management/test/MavenProfileInheritanceTest.java
+++ clever-management-web/src/test/java/edu/zju/bme/clever/management/test/MavenProfileInheritanceTest.java
@@ -8,7 +8,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = "classpath:applicationContext-test.xml")
+@ContextConfiguration(locations = "classpath:web-beans.xml")
 public class MavenProfileInheritanceTest {
 
 	@Resource(name = "mavenProfileInheritance")
