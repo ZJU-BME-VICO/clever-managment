a4a8b2e5390c2cb57d957f6d1f50ee27717f4dc6
Jacky.Lau <jacky.lauchunkin@gmail.com>
2014-12-15 17:45:46 +0800

    #13 Support jmx remote connect

--- clever-management-web/src/main/java/edu/zju/bme/clever/management/web/rest/ArchetypeResourceController.java
+++ clever-management-web/src/main/java/edu/zju/bme/clever/management/web/rest/ArchetypeResourceController.java
@@ -10,6 +10,11 @@ import org.openehr.am.archetype.Archetype;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jmx.export.annotation.ManagedAttribute;
+import org.springframework.jmx.export.annotation.ManagedResource;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,10 +22,16 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import edu.zju.bme.clever.management.service.ArchetypeValidateService;
+import edu.zju.bme.clever.management.service.ArchetypeVersionControlService;
+import edu.zju.bme.clever.management.service.UserService;
+import edu.zju.bme.clever.management.service.entity.AbstractFile.SourceType;
 import edu.zju.bme.clever.management.service.entity.FileProcessResult;
+import edu.zju.bme.clever.management.service.entity.User;
+import edu.zju.bme.clever.management.web.entity.FileUploadResult;
 import se.acode.openehr.parser.ADLParser;
 
 @RestController
+@ManagedResource
 @RequestMapping("/archetypes")
 public class ArchetypeResourceController {
 
@@ -28,8 +39,35 @@ public class ArchetypeResourceController {
 
 	@Autowired
 	private ArchetypeValidateService archetypeValidateService;
+	@Autowired
+	private ArchetypeVersionControlService archetypeVersionControlService;
+	@Autowired
+	private UserService userService;
+	
+	private boolean debug = false;
+	private boolean uploadResultManipulate;
 
-	@RequestMapping(value = "/validation", method = RequestMethod.POST)
+	@ManagedAttribute
+	public boolean isDebug() {
+		return debug;
+	}
+
+	@ManagedAttribute
+	public void setDebug(boolean debug) {
+		this.debug = debug;
+	}
+
+	@ManagedAttribute
+	public boolean isUploadResultManipulate() {
+		return uploadResultManipulate;
+	}
+
+	@ManagedAttribute
+	public void setUploadResultManipulate(boolean uploadResultManipulate) {
+		this.uploadResultManipulate = uploadResultManipulate;
+	}
+
+	@RequestMapping(value = "/action/validate", method = RequestMethod.POST)
 	public List<FileProcessResult> validateFiles(
 			@RequestParam(value = "files", required = true) MultipartFile[] files) {
 		final Map<String, Archetype> archetypes = new HashMap<String, Archetype>();
@@ -64,6 +102,44 @@ public class ArchetypeResourceController {
 		// Validate archetypes
 		this.archetypeValidateService.validateConsistency(archetypes, results);
 		return allResults;
+	}
+
+	@RequestMapping(value = "/action/upload", method = RequestMethod.POST)
+	public FileUploadResult uploadFiles(
+			@RequestParam(value = "files", required = true) MultipartFile[] files,
+			Authentication authentication) {
+		String userName = ((UserDetails) authentication.getPrincipal())
+				.getUsername();
+		FileUploadResult result = new FileUploadResult();
+		result.setSucceeded(true);
+		
+		
+		
+		User user = this.userService.getUserByName(userName);
+		Map<String, Archetype> archetypes = new HashMap<String, Archetype>();
+
+		for (MultipartFile file : files) {
+			String fileName = file.getOriginalFilename();
+			long fileSize = file.getSize();
+			this.logger.trace("Persisting file: {}, size: {}.", fileName,
+					fileSize);
+			try {
+				ADLParser parser = new ADLParser(file.getInputStream(), "UTF-8");
+				Archetype archetype = parser.parse();
+				archetypes
+						.put(archetype.getArchetypeId().getValue(), archetype);
+				this.archetypeVersionControlService.createOrUpgradeArchetype(
+						archetype, SourceType.CLEVER, user);
+			} catch (Exception ex) {
+				this.logger.debug("Persist file {} failed.",
+						file.getOriginalFilename(), ex);
+				result.setSucceeded(false);
+				result.setMessage("Persist file " + file.getOriginalFilename()
+						+ " failed.");
+				return result;
+			}
+		}
+		return result;
 	}
 
 }
