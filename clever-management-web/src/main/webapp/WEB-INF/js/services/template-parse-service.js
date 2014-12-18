17b856a6d80d4ed5836d20046022870c1222866e
wjq7712 <331400010@qq.com>
2014-12-18 22:10:08 +0800

    #11 application designer unfinished

--- clever-management-web/src/main/webapp/WEB-INF/js/services/template-parse-service.js
+++ clever-management-web/src/main/webapp/WEB-INF/js/services/template-parse-service.js
@@ -1,30 +1,121 @@
-angular.module('clever.management.services.templateParse',[]).service('templateParseService',
-function(){
-    this.parseTemplate=function(tempalte){
+angular.module('clever.management.services.templateParse',[]).service('templateParseService',function(){
+    this.parseTemplate=function(template){
     	var id=template.template_id.value;
     	var concept=template.concept;
-    	var language=this.parseLanguage(tempalte);
-    	var description=this.parseDescription(tempalte);
-    	var defination=this.parseDefination(template);
+    	var language=this.parseLanguage(template);
+    	var description=this.parseDescription(template);
+    	var definition=this.parseDefinition(template);
     	return {
-    		header:header,
+    		language:language,
     		description:description,
-    		defination:defination
+    		definition:definition
        };
     };	
     
     this.parseLanguage=function(template){
     	var language={
     		terminology:template.language.terminology_id.value,
-    		code:template.code_string,
+    		code:template.language.code_string,
     	};
-    }
-    this.parseDescription=function(template){
-    	
-    	
-    }
-    this.parseDefination=function(template){
-    	var definationTree=[];
-    	//取什么样的节点显示？
-    }
-})
+    	return language;
+    };
+    this.parseDescription=function(template){    	   	
+    };
+    this.parseDefinition=function(template){
+    	var definition={};
+    	definition.definitionTree=[];
+    	definition.contentTree=[];
+    	processNode(template.definition,undefined,undefined,definition.definitionTree,definition.contentTree);
+    	return definition;
+    };
+    function processNode(node,parent,termDefinition,treeItems,contentTree){
+    	if(angular.isArray(node)){
+    		angular.forEach(node,function(value){
+    			value.parent=parent;
+    			var extractedNode=extractNode(value,termDefinition);
+    			treeItems.push(extractedNode);
+    			termDefinition=extractedNode.label.term_definitions;
+    			if(extractedNode.label.labelContent){
+    				contentTree.push(extractedNode);
+    			}   	 			
+    			if(value.attributes){
+    				extractedNode.children=[];
+    				processNode(value.attributes,value,termDefinition,extractedNode.children,[]);
+    			}else if(value.children){
+    				extractedNode.children = [];
+					processNode(value.children, value,termDefinition,extractedNode.children,[]);
+    			}
+    		}); 
+    	}else {
+    		node.parent = parent;
+    		var extractedNode = extractNode(node,termDefinition);
+    		treeItems.push(extractedNode);
+    		termDefinition=extractedNode.label.term_definitions;
+    		if(extractedNode.label.labelContent){
+    				contentTree.push(extractedNode);
+    			}	
+			if (node.attributes) {
+				extractedNode.children = [];			
+				processNode(node.attributes, node,termDefinition, extractedNode.children,[]);
+			} else if (node.children) {
+				
+				extractedNode.children = [];
+				processNode(node.children, node,termDefinition,extractedNode.children,[]);
+			} 
+    	}
+    };
+    function extractNode (node,termDefinition) {
+      var type,attribute,code,term_definitions ,name,archtype_id;
+      type=node.rm_type_name;
+      attribute=node.rm_attribute_name;
+      if (node.node_id) {
+			code = node.node_id;
+		}
+		var label, labelType;
+		if (type) {
+			labelType = 'type';
+			label = type;
+		}
+		if (attribute) {
+			labelType = 'attribute';
+			label = attribute;
+		}
+		if (node.term_definitions) {
+			term_definitions = node.term_definitions;
+			 if(code){
+				if(term_definitions[0]==undefined){
+					name=term_definitions.items[0].__text;
+				}else{
+					angular.forEach(term_definitions,function(value){
+				      if(value._code==code){
+					   name=value.items[0].__text;}
+			});
+			}
+			}
+		}else{
+			if(termDefinition){	
+				term_definitions=termDefinition;
+			  if(code){
+				if(term_definitions[0]==undefined){
+					name=term_definitions.items[0].__text;
+				}else{
+					angular.forEach(term_definitions,function(value){
+				      if(value._code==code){
+					   name=value.items[0].__text;}
+			});
+			}
+			}}			
+		}
+		return {
+			label : {
+				type : labelType,
+				text : label,
+				code : code,
+				term_definitions : term_definitions,
+				labelContent:name
+			}
+		};
+      
+    };
+    function viewNode(){}
+});
--- clever-management-web/src/main/webapp/WEB-INF/js/views/management/application/designer/application-designer-controller.js
+++ clever-management-web/src/main/webapp/WEB-INF/js/views/management/application/designer/application-designer-controller.js
@@ -1,23 +1,14 @@
-function DesignerCtrl($scope,resourceService){
-	$scope.test = 'abc';
+function DesignerCtrl($scope,resourceService,templateParseService){
 	$scope.language = [];
 	$scope.defination={};
-	var xmlDoc;
-	var xmlhttp=new window.XMLHttpRequest();
 	var url='js/views/management/application/designer/111.xml';
-	//var url="111.xml";
-    if(xmlhttp!=null){
-
-	xmlhttp.open("GET",url,false);
-	xmlhttp.send(null);
-	}
-	if(xmlhttp.readyState==4 && xmlHttp.status == 200)
-	{
-		xmlDoc=xmlhttp.responseXML.documentElement;
-	}
-    	
+	resourceService.get(url).then(function(result){
 		var x2js = new X2JS();
-	    var template = x2js.xml_str2json(xmlDoc).template;//对象里 有没有template属性？
+	    var tmp = x2js.xml_str2json(result).template;//对象里 有没有template属性？
+	    var parseResult=templateParseService.parseTemplate(tmp);
+	});
+	//var url="111.xml";
+    	
 	//$scope.init=function(){
 		//var tmp=templateRetrieveService.getTemplateXmlByName(templateName);
 		//tmp是xml?假设是吧
