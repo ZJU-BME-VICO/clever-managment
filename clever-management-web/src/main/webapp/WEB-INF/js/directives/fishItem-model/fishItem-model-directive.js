a0ef70f59a02ed5b560460f9d803ccde0e79ff0b
cgxfx <15757118232@163.com>
2014-12-25 08:46:59 +0800

    #16   unfold  fisheye

---
+++ clever-management-web/src/main/webapp/WEB-INF/js/directives/fishItem-model/fishItem-model-directive.js
@@ -0,0 +1,57 @@
+angular.module('clever.management.directives.fishItemModel', []).directive('fishItemModel',function($compile){
+	return {
+		restrict:'E',
+		scope:{
+			funInfos:'=',
+			funNames:'=',
+			itemClick:'&',
+		},
+		transclude:'true',
+		controller:function($scope){
+			$scope.addBody=function(target){
+				//小标题头
+				$scope.funNames.push({"name":"——"});
+				while($scope.funNames.length!=2)
+					$scope.funNames.pop();
+
+				$scope.itemClick({
+					value:target
+				});
+			};
+		},
+		link: function(scope, element, attr) {
+			var template='<div class="dock" id="dock2">'+
+							'<div class="dock-container2">'+
+								'<a class="dock-item2" href="#" ng-repeat="funInfo in funInfos" item-click="addBody"'+
+									' ng-click="addBody(funInfo.alt)">'+
+									'<span>{{funInfo.title}}</span>'+
+									'<img ng-src="images/{{funInfo.imgUrl}}" alt="{{funInfo.alt}}">'+
+								'</a>'+
+							'</div>'+
+						'</div>';
+			if(scope.funInfos){
+				element.html('').append($compile(template)(scope));
+			};
+
+			scope.$watch('funInfos',function(newValue,oldValue){
+				if(newValue.length > 0){
+					$timeout(function(){
+ 						$("#dock2").Fisheye(
+							{
+								maxWidth: 60,
+								items: 'a',
+								itemsText: 'span',
+								container: '.dock-container2',
+								itemWidth: 40,
+								proximity: 80,
+								alignment : 'left',
+								valign: 'bottom',
+								halign : 'center'
+							}
+						);
+					},0);
+				}
+			});
+		}
+	};
+});
---
+++ clever-management-web/src/main/webapp/WEB-INF/js/directives/unfold-model/unfold-model-directive.js
@@ -0,0 +1,29 @@
+angular.module('clever.management.directives.unfoldModel', []).directive('unfoldModel',function($compile){
+	return{
+		restrict:'E',
+		scope:{
+			mgrFuns:'=',
+			funInfos:'=',
+			funNames:'=',
+			menuClick:'&',
+		},
+		transclude:'true',
+		controller:function($scope){
+			$scope.addFooter=function(target){
+				$scope.menuClick({
+					value:target
+				});
+			}
+		},
+		link:function(scope,element,attr){
+			var template='<a href="#" ng-repeat="mgrFun in mgrFuns" title="{{mgrFun.title}}" menu-click="addFooter"'+
+							'class="menuItem {{mgrFun.icon}} fa-2x rotary" ng-click="addFooter(mgrFun.id)">'+
+							'</a>';
+
+			if(scope.mgrFuns){
+				element.html('').append($compile(template)(scope));
+			};
+		}
+
+	};
+});
