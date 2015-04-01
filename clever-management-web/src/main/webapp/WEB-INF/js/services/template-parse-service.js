23b1fa79b916796b4ce0a83e156aae64cc15c9ad
wjq7712 <331400010@qq.com>
2015-04-01 08:56:20 +0800

    #10 modify the parse service

--- clever-management-web/src/main/webapp/WEB-INF/js/services/template-parse-service.js
+++ clever-management-web/src/main/webapp/WEB-INF/js/services/template-parse-service.js
@@ -2,58 +2,50 @@ angular.module('clever.management.services.templateParse',[]).service('templateP
     this.parseTemplate=function(template){
     	var id=template.id.value;
     	var name=template.name;
-    	var description=this.parseDescription(template);
       	var definitions=this.parseDefinition(template);// xml after deleting some node
     	return {
     		id:id,
     		name:name,
-    		description:description,
     		definitions:definitions 		
        };
     };	
-    this.parseDescription=function(template){    	   	
-    };
     this.parseDefinition=function(template){
     	var operationalTemplate=[];
     	operationalTemplate.push({template_name:template.name,template_id:template.id.value});
     	operationalTemplate.children=[];
-    	tranformOet(template.definition,undefined,operationalTemplate);    	
+    	tranformOet(template.definition,operationalTemplate);    	
     	return operationalTemplate;
     };
-    function tranformOet(node,parent,listTree){//proess oet 第一个node是definition
+    
+    function tranformOet(node,parent){//proess oet 第一个node是definition
         var nodeHere=node;
         if(angular.isArray(nodeHere)){
             angular.forEach(nodeHere,function(nodec){              
                 var archetype_name=nodec._archetype_id;
                 var childNode=[];
                 var archetype; 
-                var optTree=[];
-                optTree.parent=parent;  
+                var optTree=[]; 
                 var label={name:nodec._concept_name,archetypeName:archetype_name,code:undefined}; //root node of archetype in oet   
                 var Node=[];
                 Node.label=label;
                 resourceService.get(ARCHETYPE_BY_NAME_URL+archetype_name).then(function(archetype){
-                  parseResult = archetypeParseService.parseArchetypeXml(archetype.xml);
-                  Node.children=processRules(parseResult.definitions.treeItems,nodec);
-                  var path=nodec._path;
-                       if(path){//找cluster 原型的父节点,重新遍历书找到节点太麻烦了，，怎么处理
+                   parseResult = archetypeParseService.parseArchetypeXml(archetype.xml);
+                   Node.children=processRules(parseResult.definitions.treeItems,nodec);
+                   var path=nodec._path;
+                       if(path){//找cluster 原型的父节点,重新遍历书找到节点太麻烦了，怎么处理
                           var reg=/\[|\]|\//;
                           var path_array=path.split(reg);
                           var name=path_array.slice(-3,-2);
                           var code=path_array.slice(-2,-1);  
                           var passTree=[]; 
-                          processItems(optTree.parent,undefined,name,code,Node,passTree);
-                          Node=passTree;
+                          processItems(parent,undefined,name,code,Node,passTree);
+                          optTree=passTree;
+                        }else{
+                           optTree.push(Node);
                        }
-                  
-                  optTree.push(Node);   
-                  //if(optTree.length!=0){
-                   // listTree.push(optTree);
                     if(nodec.Items){
-                        optTree.children=[];
-                        tranformOet(nodec.Items,optTree,optTree.children);
+                        tranformOet(nodec.Items,optTree);
                     }    
-                   // }   
              });
             });       
         }else{
@@ -61,8 +53,7 @@ angular.module('clever.management.services.templateParse',[]).service('templateP
                var archetype_name=nodec._archetype_id;
                var childNode=[];
                var archetype; 
-               var optTree=[];
-               optTree.parent=parent;    
+               var optTree=[];   
                var label={name:nodec._concept_name,archetypeName:archetype_name,code:undefined}; //root node of archetype in oet   
                var Node=[];
                Node.label=label;
@@ -76,18 +67,14 @@ angular.module('clever.management.services.templateParse',[]).service('templateP
                           var name=path_array.slice(-3,-2);
                           var code=path_array.slice(-2,-1); 
                           var passTree=[]; 
-                          processItems(optTree.parent,undefined,name,code,Node,passTree);
-                          Node=passTree;
-                       }
-                  
-                  optTree.push(Node);   
-                 // if(optTree.length!=0){
-                    //listTree.push(optTree);
+                          processItems(parent,undefined,name,code,Node,passTree);
+                          optTree=passTree;
+                       }else{                  
+                        optTree.push(Node);}   
+
                     if(nodec.Items){
-                        optTree.children=[];
-                        tranformOet(nodec.Items,optTree,optTree.children);
+                        tranformOet(nodec.Items,optTree);
                     }    
-                  //  }   
              });        
          }      
     }
@@ -101,7 +88,8 @@ angular.module('clever.management.services.templateParse',[]).service('templateP
                        value.children.push(arResult);
                     }
                    }
-                  var currentNode=value.label;
+                  var currentNode=[];
+                  currentNode.label=value.label;
                   newTree.push(currentNode);
                   if(value.children){
                       currentNode.children=[];
@@ -115,7 +103,8 @@ angular.module('clever.management.services.templateParse',[]).service('templateP
                     tree.children.push(arResult);
                 }
              }
-             var currentNode=tree.label;
+             var currentNode=[];
+             currentNode.label=tree.label;
              newTree.push(currentNode);
              if(tree.children){
                  currentNode.children=[];
@@ -138,8 +127,6 @@ angular.module('clever.management.services.templateParse',[]).service('templateP
            ruleList.push({code:code,flag:flag});        
            }
        getArchetypeDetail(tree,parent,ruleList,archetypeTree);
-        /*var x2js=new X2JS(); 
-        var xmlDocStr = x2js.json2xml_str(archetypeTree);*/
         return archetypeTree;        
     }
 
@@ -215,42 +202,16 @@ angular.module('clever.management.services.templateParse',[]).service('templateP
            label : {
                 type : node.label.type,
                 text : node.label.text,
-                code:code,
-                labelContent:name,
-                dataType:dataType,
-                picType:picType,
+                code : code,
+                labelContent : name,
+                dataType : dataType,
+                picType : picType,
            }}; 
        
     }
-        function MappingTerminology(code,archetype){  
-        var name,datatype,code,archetypeName;
-        var definitions=archetypeInfo.definitions;
-        var terminologies=archetypeInfo.terminologies.terms[0].items;
-        var reg=/\[|\]|\//;
-        var path_array=path.split(reg);
-        code=path_array.slice(-2);
-        
-        if(definitions.tableItems){//只用了原型解析里解析好的数据，很多没有数据类型，考虑重新按应用模板解析方法重新写
-            if(angular.isArray(definitions.tableItems)){
-                angular.forEach(definitions.tableItems,function(value){
-                   if(value.code==code){
-                       datatype=value.type;                                   
-                   } 
-                });
-            };
-        }
-        for(i=0;i<terminologies.length;i++){
-           if(terminologies[i].code==code){
-               name=terminologies[i].text;
-           }
-       }  
-        return {
-            attrbutes:{
-                name:name,
-                datatype:datatype,
-                code:code,
-                archetypeName:archetypeName
-             }
-    };
+    
+    function MappingToTerminology(code,termDefinitions){
+        var name,dataType,picType;
+          
     }
 });
