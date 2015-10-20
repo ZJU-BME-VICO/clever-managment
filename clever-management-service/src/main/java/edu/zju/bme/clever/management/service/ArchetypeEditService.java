f5f66f90b3de803e882ad8b7fbf3f4f0f3c974bb
unknown <fdgf@ZHIYUNLONG.vico-lab.com>
2015-10-20 14:54:31 +0800

    Revert "Revert "#14 add de path show function to template edit pane""
    This reverts commit 8c79e464af766f6241221180408442d8a568bf61.

--- clever-management-service/src/main/java/edu/zju/bme/clever/management/service/ArchetypeEditService.java
+++
@@ -1,9 +0,0 @@
-package edu.zju.bme.clever.management.service;
-
-import edu.zju.bme.clever.management.service.entity.AdlInfo;
-
-public interface ArchetypeEditService {
-  public void saveArchetype(Integer id, AdlInfo archetype);
-  
-  public void submitArchetype(Integer id, AdlInfo archetype);
-}
