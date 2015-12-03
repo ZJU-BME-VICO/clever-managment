722f819ecf5bfb3adc6bff5822381a97875bfad5
lmy86263 <lmy_86263@163.com>
2015-12-03 15:06:27 -0800

    #21 update file upload record

--- clever-management-web/src/main/java/edu/zju/bme/clever/management/web/entity/FileUploadResult.java
+++ clever-management-web/src/main/java/edu/zju/bme/clever/management/web/entity/FileUploadResult.java
@@ -1,9 +1,13 @@
 package edu.zju.bme.clever.management.web.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FileUploadResult {
 
 	private boolean succeeded;
 	private String message;
+	private List<String> failTemplates = new ArrayList<>();
 
 	public boolean isSucceeded() {
 		return succeeded;
@@ -19,5 +23,17 @@ public class FileUploadResult {
 
 	public void setMessage(String message) {
 		this.message = message;
+	}
+
+	public List<String> getFailTemplates() {
+		return failTemplates;
+	}
+
+	public void setFailTemplates(List<String> failTemplates) {
+		this.failTemplates = failTemplates;
+	}
+	
+	public void addFailTemplates(String templateName){
+		this.failTemplates.add(templateName);
 	}
 }
